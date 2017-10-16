4abb272f29dad2be3ebc64c3fc1d307f737f8633
Alejandro Such Berenguer <alejandro.such@gmail.com>
2017-10-16 08:13:36 +0200

    Fix login & its effects

---
+++ src/firebase/login/FirebaseLoginErrorCodes.ts
@@ -0,0 +1,4 @@
+export class FirebaseLoginErrorCodes {
+  public static USER_NOT_FOUND: string = 'auth/user-not-found';
+  public static INVALID_PASSWORD: string = 'auth/wrong-password';
+}
--- src/firebase/login/FirebaseLoginService.ts
+++ src/firebase/login/FirebaseLoginService.ts
@@ -1,10 +1,16 @@
 import {Observable} from "rxjs/Observable";
 import {AngularFireAuth} from "angularfire2/auth";
 
+import {FirebaseLoginError} from "./FirebaseLoginError";
+import {FirebaseLoginErrorCodes} from "./FirebaseLoginErrorCodes";
 import {LoginService} from "../../domain/login/LoginService";
 import {LoginCredentials} from "../../domain/login/LoginCredentials";
+import {UserNotFoundError} from "../../domain/login/UserNotFoundError";
+import {LoginError} from "../../domain/login/LoginError";
+import {Error} from "../../domain/shared/Error";
 
 import "rxjs/add/observable/fromPromise";
+import "rxjs/add/observable/throw";
 
 export class FirebaseLoginService implements LoginService {
   constructor(private angularFireAuth: AngularFireAuth) {
@@ -12,13 +18,29 @@ export class FirebaseLoginService implements LoginService {
   }
 
   login(credentials: LoginCredentials): Observable<any> {
-    console.log('logging in with', credentials);
-
-    return Observable.fromPromise(
-      this.angularFireAuth
-        .auth
-        .signInWithEmailAndPassword(credentials.email, credentials.password)
-    )
+    return Observable
+      .fromPromise(
+        this.angularFireAuth
+          .auth
+          .signInWithEmailAndPassword(credentials.email, credentials.password)
+      )
+      .catch(error => FirebaseLoginService.onFirebaseLoginError(error, credentials));
   }
 
+  private static onFirebaseLoginError(error: FirebaseLoginError, credentials: LoginCredentials): Observable {
+    let result: Observable<Error> = null;
+
+    switch (error.code) {
+      case FirebaseLoginErrorCodes.USER_NOT_FOUND:
+        result = Observable.throw(new UserNotFoundError(credentials));
+        break;
+      case FirebaseLoginErrorCodes.INVALID_PASSWORD:
+        result = Observable.throw(new LoginError('Contraseña incorrecta.', error.code));
+        break;
+      default:
+        result = Observable.throw(new LoginError('Se ha producido un error.', error.code));
+    }
+
+    return result;
+  }
 }
