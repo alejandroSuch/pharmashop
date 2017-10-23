a266dd5b05270fe5fa08b75414ceeb5df71e7e53
Alejandro Such Berenguer <alejandro.such@gmail.com>
2017-10-23 08:25:13 +0200

    Rename module

--- src/app/context/FirestoreContextModule.ts
+++ src/app/context/FirestoreContextModule.ts
@@ -20,12 +20,11 @@ import { LOGIN_SERVICE, LOGIN_USE_CASE } from '../InjectionTokens';
       provide: LOGIN_USE_CASE,
       deps: [LOGIN_SERVICE],
       useFactory: (loginService: LoginService) => {
-        debugger;
         return new Login(loginService);
       }
     }
   ]
 })
-export class ApplicationContextModule {
+export class FirestoreContextModule {
 
 }
